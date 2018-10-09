64a6433c13441f05cc51980c0e201be4e08dd65b
Cole Bemis <colebemis@gmail.com>
2018-10-09 12:37:20 -0700

    Accept css prop

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import { object } from 'prop-type'
 import styled from 'styled-components'
 
-const Base = styled.div({})
+const Base = styled.div(props => props.css)
+
+Base.propTypes = {
+  /** Style object */
+  css: object,
+}
 
 export default Base
