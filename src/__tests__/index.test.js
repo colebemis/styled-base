812f9a71c42213a3c63afa4c0bb35c20443de967
Cole Bemis <colebemis@gmail.com>
2018-10-14 22:01:40 -0700

    Evaluate theme keys

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,118 @@
+import React from 'react'
+import { render } from 'react-testing-library'
+import { ThemeProvider } from 'styled-components'
+import Base from '..'
+
+it('accepts an `as` prop to change the rendered DOM element', () => {
+  const { container } = render(<Base as="span">Hello World</Base>)
+
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+it('accepts a `css` prop', () => {
+  const { container } = render(<Base css={{ color: 'red' }}>Hello World</Base>)
+
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+it('accepts a `theme` prop', () => {
+  const theme = {
+    colors: {
+      blue: '#07f',
+    },
+  }
+
+  const { container } = render(
+    <Base theme={theme} css={{ color: '$colors.blue' }}>
+      Hello World
+    </Base>,
+  )
+
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+describe('`css` prop', () => {
+  it('supports nesting', () => {
+    const { container } = render(
+      <Base
+        css={{
+          boxSizing: 'border-box',
+          '.foo': { color: 'red' },
+          '& *, & *::before, & *::after': { boxSizing: 'inherit' },
+        }}
+      >
+        Hello World
+      </Base>,
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('supports pseudo-classes and pseudo-elements', () => {
+    const { container } = render(
+      <Base
+        css={{
+          color: 'red',
+          '&:hover': { color: 'blue' },
+          '&::before': { content: '"🌎"' },
+        }}
+      >
+        Hello World
+      </Base>,
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('evaluates theme keys prefixed with "$"', () => {
+    const theme = {
+      spacing: ['0', '4px', '8px', '16px', '24px', '32px'],
+      colors: {
+        blue: '#07f',
+        gray: '#ccc',
+      },
+    }
+
+    const { container } = render(
+      <ThemeProvider theme={theme}>
+        <Base
+          css={{
+            display: 'inline-block',
+            padding: '$spacing.1 $spacing.2',
+            margin: '-$spacing.1 auto',
+            color: '$colors.blue',
+            backgroundColor: '$foo',
+          }}
+        >
+          Hello World
+        </Base>
+      </ThemeProvider>,
+    )
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  // it('supports responsive values', () => {
+  //   const theme = {
+  //     spacing: ['0', '4px', '8px', '16px', '24px', '32px'],
+  //     fontSizes: ['12px', '14px', '16px', '20px', '24px'],
+  //     breakpoints: ['576px', '768px', '992px'],
+  //   }
+
+  //   const { container } = render(
+  //     <ThemeProvider theme={theme}>
+  //       <Base
+  //         css={{
+  //           width: ['100%', '50%'],
+  //           padding: ['$spacing.0', '$spacing.1', '$spacing.2', '$spacing.3'],
+  //           fontSizes: [null, '$fontSizes.0', null, '$fontSizes.1', '$fontSizes.2'],
+  //         }}
+  //       >
+  //         Hello World
+  //       </Base>
+  //     </ThemeProvider>,
+  //   )
+
+  //   expect(container.firstChild).toMatchSnapshot()
+  // })
+})
--- src/index.js
+++ src/index.js
@@ -1,7 +1,41 @@
 import { object } from 'prop-types'
 import styled from 'styled-components'
 
-const Base = styled.div(props => props.css)
+const Base = styled.div(props => transform(props.css, props.theme))
+
+function transform(css, theme) {
+  if (!css) return null
+
+  return Object.entries(css).reduce((acc, [property, value]) => {
+    switch (typeof value) {
+      case 'object':
+        acc[property] = transform(value, theme)
+        break
+
+      case 'string':
+        acc[property] = supplant(value, theme)
+        break
+
+      default:
+        acc[property] = value
+    }
+
+    return acc
+  }, {})
+}
+
+function supplant(string, obj) {
+  return string.replace(
+    /\$[(a-z)(A-Z)(0-9)(.)]+/g,
+    match => get(match.slice(1), obj) || match,
+  )
+}
+
+function get(path, obj) {
+  return path
+    .split('.')
+    .reduce((acc, key) => (acc && acc[key] ? acc[key] : null), obj)
+}
 
 Base.propTypes = {
   /** Style object */
